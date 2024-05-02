4c36c4921db3cdf6a5dc38439b2b94b4107d1693
dideey <bradleyjd32@gmail.com>
2024-05-03 01:07:28 +0300

    mixed list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-from typing import List
+from typing import List, Union
 """importing list"""
 
-mytype = tuple[int, float]
+mytype = List[Union[int, float]]
+
+
 def sum_mixed_list(mxd_list: mytype) -> float:
     total = 0
     for item in mxd_list:
