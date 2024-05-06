50ff07b155c1544aa94ca42848732891fd422296
dideey <bradleyjd32@gmail.com>
2024-05-06 17:37:23 +0300

    change

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 """import asyncio and random modules"""
 
 async def wait_random(max_delay: int = 10):
-    """takes an interger and waits for a random number of seconds between 0 and the integer value passed as argument."""
+    """takes an interger and waits for a random number of seconds"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
